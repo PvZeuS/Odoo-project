d54af1a8510529931e2a2148444962c01a842000
Piero Valle <pierovallezapata01@gmail.com>
2026-03-26 13:14:19 -0500

    MOD:modulo_prueba despliegue probando error- test unit

--- addons/modulo_prueba/__init__.py
+++ addons/modulo_prueba/__init__.py
@@ -1,5 +1,2 @@
 from . import models
 from . import tests
-
-# Error de variable no definida y espacios extra
-x = variable_que_no_existe
--- addons/modulo_prueba/tests/test_prueba.py
+++ addons/modulo_prueba/tests/test_prueba.py
@@ -3,4 +3,6 @@ from odoo.tests.common import TransactionCase
 class TestModuloPrueba(TransactionCase):
     def test_01_verificacion_modelo(self):
         # Verificamos que el modelo existe en el registro de Odoo
-        self.assertIn('modulo.prueba', self.env, "El modelo 'modulo.prueba' no se registró correctamente")
+        #self.assertIn('modulo.prueba', self.env, "El modelo 'modulo.prueba' no se registró correctamente")
+        # Si antes tenías self.assertEqual(1, 1)
+        self.assertEqual(1, 2, "Error provocado: 1 no es 2")
