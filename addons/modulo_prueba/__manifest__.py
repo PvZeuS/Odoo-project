50068cf9e3a7cd56de84a3edc05b5c634b67edbd
Piero Valle <pierovallezapata01@gmail.com>
2026-03-26 19:07:32 -0500

    MOD:modulo_prueba prueba Rollback

--- addons/modulo_prueba/__manifest__.py
+++ addons/modulo_prueba/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Módulo de Prueba Automatización staging ROLLBACK',
+    'name': 'Módulo de Prueba Automatización staging ROLLBACK-1',
     'version': '1.0',
     'category': 'Extra Tools',
     'summary': 'Módulo simple para verificar la carga',
